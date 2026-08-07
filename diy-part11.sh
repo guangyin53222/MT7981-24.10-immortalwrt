b53c4a14c94d545ac60d558665522cea15064a22
guangyin53222 <532225662@qq.com>
2026-08-07 13:59:25 +0800

    Update diy-part11.sh

--- diy-part11.sh
+++ diy-part11.sh
@@ -9,5 +9,23 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
+# ===================== 第三方插件统一拉取（先删旧残留） =====================
+rm -rf package/OpenAppFilter
+git clone --depth 1 -b v6.1.8 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 
+rm -rf package/luci-app-store
+git clone --depth=1 https://github.com/linkease/istore.git package/luci-app-store
+
+rm -rf package/luci-app-gecoosac
+git clone --depth=1 https://github.com/laipeng668/luci-app-gecoosac package/luci-app-gecoosac
+
+rm -rf tmp/openwrt-app-actions package/luci-app-wan-mac
+git clone --depth=1 https://github.com/linkease/openwrt-app-actions tmp/openwrt-app-actions
+mv tmp/openwrt-app-actions/applications/luci-app-wan-mac package/luci-app-wan-mac
+rm -rf tmp/openwrt-app-actions
+# 添加 luci-app-tcpdump 抓包插件
+git clone https://github.com/KFERMercer/luci-app-tcpdump.git ./package/luci-app-tcpdump
+# 更新并安装插件源
+./scripts/feeds update -a
+./scripts/feeds install -a
 
